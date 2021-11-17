8225ec9cd4b8dccba15b6d115e5395c1047798b0
Ravindu Rasanga <it20189730@my.sliit.lk>
2021-11-17 23:20:09 +0530

    JS Function Integrated - V1
    JS Password generator main function integrated

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function getpassword(){
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRXYZ*$#%@&";
+
+    var chars = "@$0123456789$%abcdefghijklmnopqrstuvwxyz&*ABCDEFGHIJKLMNOPQRXYZ*^+";
+    var passwordLength = 10;
+    var password = "";
+
+    for(var i=0; i<passwordLength; i++){
+        var randomNo = Math.floor(Math.random() * chars.length);
+
+        password += chars.substring(randomNo, randomNo + 1);
+
+        document.getElementById("Password").value = password;
+    }
     
 
 }
