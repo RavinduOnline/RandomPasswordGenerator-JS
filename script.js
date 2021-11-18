37879c8239082d383f747700ee1f16b231fa7609
Ravindu Rasanga <it20189730@my.sliit.lk>
2021-11-18 18:25:52 +0530

    Improved some designs  & JS Function DV3 - FV2
    Password Copied  Function Integrated

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+//Password Generate Function 
 function getpassword(){
 
-    var chars = "@$0123456789$%abcdefghijklmnopqrstuvwxyz&*ABCDEFGHIJKLMNOPQRXYZ*^+";
+    //Create Characters & Password length
+    var chars = "@$0123456789$%abcdefghijklmnopqrstuvwxyz&*ABCDEFGHIJKLMNOPQRXYZ*!+";
     var passwordLength = 10;
     var password = "";
 
+    //generate password using for loop
     for(var i=0; i<passwordLength; i++){
         var randomNo = Math.floor(Math.random() * chars.length);
 
@@ -11,6 +15,30 @@ function getpassword(){
 
         document.getElementById("Password").value = password;
     }
-    
+}
+
+//Password Copy Function 
+function copyPassword(){
+    var copyText = document.getElementById("Password");
+
+    //Check Password 
+    if(copyText.value.length == 0){
+        document.getElementById("alert-box").style.color = "red"
+        document.getElementById("alert-box").innerHTML = "Password Not Generated";
+    }
+
+    else{
+
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(copyText.value);
+        document.getElementById("alert-box").style.color = "white"
+        document.getElementById("alert-box").innerHTML = "New Password Copied";
+
+    }
+
+    setTimeout(function(){
+        document.getElementById("alert-box").innerHTML = "";
+        }, 2000);
 
 }
